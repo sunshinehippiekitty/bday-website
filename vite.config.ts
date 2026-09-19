a1c396cc30c47ac49cb2ce12eebef304bf50471b
sunshinehippiekitty <sunshinehippiekitty@gmail.com>
2026-09-20 00:03:12 +0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
+  base: '/bday-website/',
 })
+
